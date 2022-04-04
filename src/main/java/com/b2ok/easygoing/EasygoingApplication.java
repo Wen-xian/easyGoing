6e03c06fc82e350e30c135c25b10d73289a6d20b
wenxian <xian.wen@unionman.com.cn>
2022-04-04 20:00:11 +0800

    上传基础项目文件

---
+++ src/main/java/com/b2ok/easygoing/EasygoingApplication.java
@@ -0,0 +1,13 @@
+package com.b2ok.easygoing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasygoingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasygoingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/b2ok/easygoing/EasygoingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2ok.easygoing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasygoingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
